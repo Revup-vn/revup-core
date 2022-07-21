c2d2370e40db757ed651bf884a7fe24c3630fdb9
Conrad Hoang <tcmhoang@outlook.com>
2022-07-21 05:57:17 -0400

    feat(store): implement repair service store

--- lib/src/shared/const.dart
+++ lib/src/shared/const.dart
@@ -3,3 +3,4 @@ const kPathPayAccountsDocument = 'pay_accounts';
 const kPathRecordCollection = 'record';
 const kPathPaymentDocument = 'payment';
 const kPathProviderCategoryDocument = 'category';
+const kPathProviderCategoryServiceDocument = 'service';
